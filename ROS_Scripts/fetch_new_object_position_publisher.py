ed0c3eb5b9a1cca2d5b168e102a1a8ff6c4057fe
Philipp <psypdt@nottingham.ac.uk>
2020-02-27 20:18:43 +0000

    refactor add_object_pose_node filename and class

--- ROS_Scripts/fetch_new_object_position_publisher.py
+++ ROS_Scripts/fetch_new_object_position_publisher.py
@@ -9,31 +9,27 @@ from intera_interface import CHECK_VERSION
 
 
 
-class ObjectLocalization:
-
+class PositionFetcherPublisher:
 
     def __init__(self):
         rospy.init_node('live_item_pose_node')  # Used to add new items into the scene
         self.has_finished = False
         self.done_pub = rospy.Publisher('/live_pose_node/object/final_pose', Pose, queue_size=10)  # Publishes final position of item for ui to add into xml
         self.pose_sub = rospy.Subscriber('/ik/new_object/live/pose', Pose, callback=self.send_final_to_ui, queue_size=10)  # Get current pose of arm
-        self.signal_done_sub = rospy.Subscriber('/ui/new_object/state/done', Bool, callback=self.is_done_callback, queue_size=10)  # UI tells us that it has finished succesfully finding the obj & wants the position
+        self.signal_done_sub = rospy.Subscriber('/ui/new_object/state/done', Bool, callback=self.assign_final_position_callback, queue_size=10)  # UI tells us that it has finished succesfully finding the obj & wants the position
         
         rospy.spin()
 
 
 
-    ##  Trivial callback that assigns the received pose to a member variable
-    def is_done_callback(self, state):
+    ##  Callback that assigns the received pose to a member variable
+    def assign_final_position_callback(self, state):
         self.has_finished = state.data
-        
-        # self.done_pub.publish(self.current_pose)
 
 
 
     ##  This callback will send the final pose to the UI
     def send_final_to_ui(self, data):
-        
         if self.has_finished:
             self.done_pub.publish(data)
 
@@ -41,4 +37,4 @@ class ObjectLocalization:
 
 
 if __name__ == "__main__":
-    ol = ObjectLocalization()
+    pfp = PositionFetcherPublisher()

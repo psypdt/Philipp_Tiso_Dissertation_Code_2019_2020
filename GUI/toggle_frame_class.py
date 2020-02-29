119252c4969970363d9e7aa18d5bb09188b29529
Philipp <psypdt@nottingham.ac.uk>
2020-02-29 21:55:14 +0000

    Fix incorrect state symbol on toggle, symbol was ^, should have been +

--- GUI/toggle_frame_class.py
+++ GUI/toggle_frame_class.py
@@ -26,7 +26,7 @@ class ToggledBatchFrame(tk.Frame):
         # ttk.Label(self.title_frame, text=self.batch_text).pack(side="left", fill="x", expand=1)
 
         #  Create button which will toggle the view
-        self.toggle_button = ttk.Checkbutton(self.title_frame, text=str(self.batch_text + ' ^'), command=self.toggle_state, variable=self.show, style='Toolbutton')
+        self.toggle_button = ttk.Checkbutton(self.title_frame, text=str(self.batch_text + ' +  '), command=self.toggle_state, variable=self.show, style='Toolbutton')
         self.toggle_button.pack(side="left", expand=1, fill='x', anchor='w')
         
 
@@ -39,7 +39,7 @@ class ToggledBatchFrame(tk.Frame):
     def toggle_state(self):
         if bool(self.show.get()):
             self.sub_frame.pack(fill="x", expand=1)
-            self.toggle_button.configure(text=str(self.batch_text + ' ^'))
+            self.toggle_button.configure(text=str(self.batch_text + ' ^  '))
         else:
             self.sub_frame.forget()
-            self.toggle_button.configure(text=str(self.batch_text + ' +'))
+            self.toggle_button.configure(text=str(self.batch_text + ' +  '))
